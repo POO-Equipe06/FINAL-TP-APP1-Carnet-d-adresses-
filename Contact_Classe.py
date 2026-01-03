3731d5aa4b22f30c5a8578e62864ec5262eb3231
ibranar193 <mouadkadmiri@gmail.com>
2026-01-02 19:24:40 -0500

    new contact

--- Contact_Classe.py
+++ Contact_Classe.py
@@ -1,11 +1,13 @@
-# Class pour les Contact
+# Classe pour les Contact
 
 class Contact :
+
     def __init__(self, nom:str , prenom:str , telephone:int , email:str ):
         self.nom = nom
         self.prenom = prenom
         self.telephone = telephone
         self.email = email
-    def Contact_list(self):
-        contact=[self.nom, self.prenom, self.telephone, self.email]
-        return contact
+
+    def contact_en_list(self):
+        c=[self.nom, self.prenom, self.telephone, self.email]
+        return f"{c[0]} {c[1]} | {c[2]} | {c[3]}"
